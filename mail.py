059c1a26e8c6e9a276ba4c4702713e507b68eb22
Nischal1020 <nischal.khadka@mail.bcu.ac.uk>
2023-10-19 01:52:59 -0600

    nis

---
+++ mail.py
@@ -0,0 +1,17 @@
+import smtplib
+
+try:
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    # Use your App Password here
+    server.login('cpboys11@gmail.com', 'Cpboys12345')
+
+    server.sendmail('cpboys11@gmail.com', 'technicalnazwa@gmail.com', 'this is a fake mail')
+    print('Mail sent successfully')
+
+except Exception as e:
+    print('An error occurred:', str(e))
+
+finally:
+    server.quit()
